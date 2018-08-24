d75ce98c1ff502e114d8e5d2fd69a04568ef6ff2
Patric O'Hara <patrico@op-patricko-mbp.local>
2018-08-24 15:54:00 +1000

    asd

--- App.js
+++ App.js
@@ -258,7 +258,7 @@ export default class App extends Component<Props> {
             userInfo: {id:3},
             message: 'This is a Mr. Sleepy Test Alarm', // (required)
             playSound: true, // (optional) default: true
-            soundName: 'prodsleepy.wav', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+            soundName: 'alarm.wav', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
             // repeatType:`${alarm.frequency[0].option === 'Everyday' ? 'day' : ''}`, // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
             // repeatTime: , //should the number of milliseconds between each interval.
             // actions: '["Snooze", "Stop"]',
