cbaedb5fcf39a96e19734c8e088e4cf353d8e510
Patric O'Hara <patrico@op-patricko-mbp.local>
2018-09-17 13:47:05 +1000

    asd

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -30,33 +30,6 @@ class Login extends Component<{}> {
         }
     }
 
-    componentDidMount(){
-        if (Platform.OS === 'android') {
-            Linking.getInitialURL().then(url => {
-              this.navigate(url);
-            })
-        } else {
-            Linking.addEventListener('url', this.handleOpenURL);
-        }
-    }
-
-    componentWillUnmount() { // C
-        Linking.removeEventListener('url', this.handleOpenURL);
-    }
-    handleOpenURL = (event) => { // D
-        this.navigate(event.url);
-    }
-    navigate = (url) => { // E
-        console.log('URL',url)
-        // const { navigate } = this.props.navigation;
-        // const route = url.replace(/.*?:\/\//g, '');
-        // const id = route.match(/\/([^\/]+)\/?$/)[1];
-        // const routeName = route.split('/')[0];
-        // if (routeName === 'people') {
-        //   navigate('People', { id, name: 'chris' })
-        // };
-    }
-
     closeToLogin = () => {
         this.props.toggleLoading()
         this.setState({login:true})
